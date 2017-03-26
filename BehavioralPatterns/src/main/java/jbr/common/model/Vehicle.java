7fa3741c3a4645e963034e5c2524b8dfb22f8daf
Ranjith Sekar <java2ranjith@gmail.com>
2017-03-26 23:04:23 +0530

    behavioral pattern common

---
+++ BehavioralPatterns/src/main/java/jbr/common/model/Vehicle.java
@@ -0,0 +1,56 @@
+package jbr.common.model;
+
+import jbr.common.VehicleType;
+
+public class Vehicle {
+  private int noOfWheels;
+  private String manufacturer;
+  private VehicleType vehicleType;
+  private int price;
+
+  public Vehicle(int noOfWheels, String manufacturer, VehicleType vehicleType, int price) {
+    this.noOfWheels = noOfWheels;
+    this.manufacturer = manufacturer;
+    this.vehicleType = vehicleType;
+    this.price = price;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
+  }
+
+  public VehicleType getVehicleType() {
+    return vehicleType;
+  }
+
+  public void setVehicleType(VehicleType vehicleType) {
+    this.vehicleType = vehicleType;
+  }
+
+  public int getNoOfWheels() {
+    return noOfWheels;
+  }
+
+  public void setNoOfWheels(int noOfWheels) {
+    this.noOfWheels = noOfWheels;
+  }
+
+  public String getManufacturer() {
+    return manufacturer;
+  }
+
+  public void setManufacturer(String model) {
+    this.manufacturer = model;
+  }
+
+  @Override
+  public String toString() {
+
+    return "Type: " + this.vehicleType + "\nNo Of Wheels: " + this.noOfWheels + "\nManufacturer: " + this.manufacturer
+        + "\nPrice: " + this.price;
+  }
+}
