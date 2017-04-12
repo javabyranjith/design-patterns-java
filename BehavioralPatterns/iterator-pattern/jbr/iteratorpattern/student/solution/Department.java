d8e79b0a553b0dfa806ec7021a2ae3fb17654d1a
Ranjith Sekar <java2ranjith@gmail.com>
2017-04-12 22:43:56 +0530

    Iterator Design Pattern with Student Example

---
+++ BehavioralPatterns/iterator-pattern/jbr/iteratorpattern/student/solution/Department.java
@@ -0,0 +1,5 @@
+package jbr.iteratorpattern.student.solution;
+
+public interface Department {
+  StudentIterator getStudents();
+}
---
+++ BehavioralPatterns/iterator-pattern/jbr/iteratorpattern/student/solution/StudentIterator.java
@@ -0,0 +1,8 @@
+package jbr.iteratorpattern.student.solution;
+
+public interface StudentIterator {
+
+  boolean hasNext();
+
+  Object next();
+}
